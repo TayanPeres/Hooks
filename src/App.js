5c215e2317da5430aba4052b799d28bd9b2b9a38
Tayane <tayperess@hotmail.com>
2021-05-29 14:46:17 -0300

    estudando hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo, useCallback} from 'react';
 
-function App() {
+function App () {
+  const [tarefas, setTarefas] = useState ([ ]) 
+  const [input, setInput] = useState('') 
+
+   //buscando tarefas 
+  useEffect(() => {
+    const tarefaStorage = localStorage.getItem('tarefas')
+    //
+    if(tarefaStorage) {
+      setTarefas(JSON.parse(tarefaStorage))
+    }
+ 
+  }, [])
+//toda vez que a tarefas sofrer auteraçoes,
+    //vai chamar a funçao , adicionar todos os arrays
+    //salvando
+  useEffect(() => {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+  }, [tarefas])
+
+ const handleAdd = useCallback(() =>  {
+   setTarefas([...tarefas, input ])
+   setInput('')
+ }, [input, tarefas])
+
+ const totalFrase = useMemo(() => tarefas.length, [tarefas])
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+      <br />
+      <strong>voce tem {totalFrase} tarefas</strong><br />
+      <input type="text" value={input} 
+      onChange={ (e) => setInput(e.target.value)} />
+      <button type="button" onClick={handleAdd}>Adicionar</button>
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
